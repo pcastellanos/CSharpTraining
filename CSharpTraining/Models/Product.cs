0cd739a4bf39b778ce91748292579858ebe2367f
pcastellanos <pcastellanos90@gmail.com>
2016-01-23 14:52:56 -0500

    Listing C# 1

---
+++ CSharpTraining/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining.Models
+{
+    public class Product
+    {
+        string name;
+
+        //public getter properties,then public setter too
+        public string Name { get { return name; } set { name = value; } }
+
+        double price;
+
+        public double Price { get { return price; } }
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSharpTraining.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,29 @@ namespace CSharpTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
+            ArrayList products = GetSampleProducts();
+            int size = products.Count;
+            for(int i=0; i<size; i++)
+            {
+                Console.WriteLine(products[i].ToString());
+            }
+            Console.ReadLine();
         }
+
+        private static ArrayList GetSampleProducts()
+        {
+            //Arraylist accept all kind of objects
+            ArrayList list = new ArrayList();
+            list.Add(new Product("West Side Story", 9.99));
+            list.Add(new Product("Assassins", 14.99));
+            list.Add(new Product("Frogs", 13.99));
+            list.Add(new Product("Sweeney Todd", 10.99));
+            //list.Add("Adición de un texto normal");
+            //list.Add(12432424);
+            return list;
+        }
+
     }
 }
