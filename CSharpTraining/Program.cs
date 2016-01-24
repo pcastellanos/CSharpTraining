7f81a598517610ade7562ed9ac3516262a0f6048
pcastellanos <pcastellanos90@gmail.com>
2016-01-24 11:12:59 -0500

    Using Linq C#3

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -11,15 +11,17 @@ namespace CSharpTraining
         public static void Main(string[] args)
         {
             List<Product> products = GetSampleProducts();
-            Action<Product> write = Console.WriteLine;
 
-            //Using lambda expression
-            Console.WriteLine("Products with unknown price");
-            products.FindAll(delegate (Product p) { return !p.Price.HasValue; }).ForEach(write);
-            Console.Write("\n\n");
-            Console.WriteLine("Products with known price");
-            products.FindAll(product => product.Price.HasValue && product.Price > 10).ForEach(write);
-            Console.Write("\n\n");
+            //Linq
+            var filtered = from Product product in products
+                           where product.Price > 10
+                           select product;
+
+            foreach (Product product in filtered)
+            {
+                Console.WriteLine(product);
+            }
+
             Console.ReadLine();
         }
 
@@ -28,12 +30,12 @@ namespace CSharpTraining
             //Arraylist accept all kind of objects
             List<Product> list = new List<Product>();
             list.Add(new Product("West Side Story 2", 13.76));
-            list.Add(new Product("West Side Story"));
+            list.Add(new Product("West Side Story", 9.99));
             list.Add(new Product("Frogs"));
             list.Add(new Product("Assassins", 14.99));
             list.Add(new Product("Assassins 2"));
             list.Add(new Product("Sweeney Todd", 10.99));
-            list.Add(new Product("Sweeney Todd"));
+            list.Add(new Product("Sweeney Todd", 5.87));
             return list;
         }
 
