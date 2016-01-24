6ff9d5eac53fa7f59f0b08800f86a44b4f9410a6
pcastellanos <pcastellanos90@gmail.com>
2016-01-24 10:18:59 -0500

    Strong coupling between condition and action C#1

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -9,31 +9,29 @@ namespace CSharpTraining
     {
         public static void Main(string[] args)
         {
-            List<Product> products = GetSampleProducts();
-
-            //lambda expression. Implicit type (first, second)
-            //products.Sort((first, second) => first.Name.CompareTo(second.Name));
-            products.Sort((first, second) => first.Price.CompareTo(second.Price));
+            ArrayList products = GetSampleProducts();
 
             foreach (Product product in products)
             {
-                Console.WriteLine(product + " is " + product.TellAboutPrice());
+                if (product.Price >= 10)
+                    Console.WriteLine(product);
             }
             Console.Write("\n\n");
             
             Console.ReadLine();
         }
 
-        private static List<Product> GetSampleProducts()
+        private static ArrayList GetSampleProducts()
         {
             //Arraylist accept all kind of objects
-            List<Product> list = new List<Product>();
+            ArrayList list = new ArrayList();
+            list.Add(new Product("West Side Story 2", 13.76));
             list.Add(new Product("West Side Story", 9.99));
-            list.Add(new Product("Assassins", 14.99));
             list.Add(new Product("Frogs", 13.99));
+            list.Add(new Product("Assassins", 14.99));
+            list.Add(new Product("Assassins 2", 24.23));
             list.Add(new Product("Sweeney Todd", 10.99));
-            //list.Add("Adición de un texto normal");
-            //list.Add(12432424);
+            list.Add(new Product("Sweeney Todd", 5.99));
             return list;
         }
 
