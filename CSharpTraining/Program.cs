64cd2d33ebe720c4cad033f3fabf90c86f37bf68
pcastellanos <pcastellanos90@gmail.com>
2016-02-28 16:26:17 -0500

    Let clause

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -11,32 +11,40 @@ using System.Xml.Linq;
 namespace CSharpTraining
 {
     class Program
-    {        
+    {
         public static void Main(string[] args)
         {
-            User tim = SampleData.Users.TesterTim;
-            var query = from defect in SampleData.AllDefects
-                        where defect.Status != Status.Closed
-                        where defect.AssignedTo == tim
-                        orderby defect.Severity descending, defect.LastModified //ascending
-                        select defect;
-            foreach (var defect in query)
+            var query = from user in SampleData.AllUsers
+                        orderby user.Name.Length
+                        select user.Name;
+            foreach (var name in query)
             {
-                Console.WriteLine("{0}: {1} {2}", defect.Severity, defect.Summary, defect.LastModified.ToShortDateString());
+                Console.WriteLine("{0}: {1}", name.Length, name);
             }
-            
             Console.ReadLine();
 
+            //Console.WriteLine("Using let");
+            //var users = from user in SampleData.AllUsers
+            //            let length = user.Name.Length
+            //            orderby length
+            //            select new { Name = user.Name, Length = length };
+
+            //foreach (var user in users)
+            //{
+            //    Console.WriteLine("{0}: {1}", user.Length, user.Name);
+            //}
+            //Console.ReadLine();
+
             //translated
-            var defects = SampleData.AllDefects.Where(defect => defect.Status != Status.Closed)
-                                 .Where(defect => defect.AssignedTo == tim)
-                                 .OrderByDescending(defect => defect.Severity)
-                                 .ThenBy(defect => defect.LastModified);
-            foreach (var defect in defects)
-            {
-                Console.WriteLine("{0}: {1} {2}", defect.Severity, defect.Summary, defect.LastModified.ToShortDateString());
-            }
-            Console.ReadLine();
+            //var resultUsers = SampleData.AllUsers.Select(user => new { user, length = user.Name.Length })
+            //        .OrderBy(user => user.length)
+            //        .Select(anonymousUser => new { Name = anonymousUser.user.Name, Length = anonymousUser.length });
+
+            //foreach (var user in resultUsers)
+            //{
+            //    Console.WriteLine("{0}: {1}", user.Length, user.Name);
+            //}
+            //Console.ReadLine();
         }       
     }
 }
