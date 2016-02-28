0da3907578c69276ce2caa0d7a8af7d01acc34b4
pcastellanos <pcastellanos90@gmail.com>
2016-02-28 16:02:55 -0500

    Ordering a sequence

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -11,37 +11,32 @@ using System.Xml.Linq;
 namespace CSharpTraining
 {
     class Program
-    {
-        
+    {        
         public static void Main(string[] args)
         {
             User tim = SampleData.Users.TesterTim;
-            var results = from defect in SampleData.AllDefects
+            var query = from defect in SampleData.AllDefects
                         where defect.Status != Status.Closed
                         where defect.AssignedTo == tim
-                        select defect.Summary;
-            foreach (var summary in results)
+                        orderby defect.Severity descending, defect.LastModified //ascending
+                        select defect;
+            foreach (var defect in query)
             {
-                Console.WriteLine(summary);
+                Console.WriteLine("{0}: {1} {2}", defect.Severity, defect.Summary, defect.LastModified.ToShortDateString());
             }
-            Console.ReadLine();
-            //The query expression is translated into this
-            IEnumerable<string> summaries = SampleData.AllDefects.Where(defect => defect.Status != Status.Closed)
-                                                                 .Where(defect => defect.AssignedTo == tim)
-                                                                 .Select(defect => defect.Summary);
-            foreach (var summary in summaries)
-            {
-                Console.WriteLine(summary);
-            }
+            
             Console.ReadLine();
 
-
-        }
-
-
-
-      
-
-
+            //translated
+            var defects = SampleData.AllDefects.Where(defect => defect.Status != Status.Closed)
+                                 .Where(defect => defect.AssignedTo == tim)
+                                 .OrderByDescending(defect => defect.Severity)
+                                 .ThenBy(defect => defect.LastModified);
+            foreach (var defect in defects)
+            {
+                Console.WriteLine("{0}: {1} {2}", defect.Severity, defect.Summary, defect.LastModified.ToShortDateString());
+            }
+            Console.ReadLine();
+        }       
     }
 }
