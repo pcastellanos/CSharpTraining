e1fc2793e1d4b46681087f8a69ad21c09b9fc570
pcastellanos <pcastellanos90@gmail.com>
2016-02-28 18:16:53 -0500

    Group joins with join...into clauses

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -15,34 +15,43 @@ namespace CSharpTraining
         public static void Main(string[] args)
         {
             var query = from defect in SampleData.AllDefects
+                        where defect.Status == Status.Closed
                         join subscription in SampleData.AllSubscriptions
                         on defect.Project equals subscription.Project
-                        select new { defect.Summary, subscription.EmailAddress };
-
-            //var query = from subscription in SampleData.AllSubscriptions
-            //            join defect in SampleData.AllDefects
-            //            on subscription.Project equals defect.Project 
-            //            select new { defect.Summary, subscription.EmailAddress };
-
-            //var query = from defect in SampleData.AllDefects
-            //            where defect.Status == Status.Closed
-            //            join subscription in SampleData.AllSubscriptions
-            //            on defect.Project equals subscription.Project
-            //            select new { defect.Summary, subscription.EmailAddress };
-
-            //var query = from subscription in SampleData.AllSubscriptions
-            //join defect in (from defect in SampleData.AllDefects
-            //                where defect.Status == Status.Closed
-            //                select defect)
-            //on subscription.Project equals defect.Project
-            //select new { defect.Summary, subscription.EmailAddress };
-
+                        into groupedSubscriptions
+                        select new
+                        {
+                            Defect = defect,
+                            Subscriptions = groupedSubscriptions
+                        };
             foreach (var entry in query)
             {
-                Console.WriteLine("{0}: {1}", entry.EmailAddress, entry.Summary);
+                Console.WriteLine(entry.Defect.Summary);
+                foreach (var subscription in entry.Subscriptions)
+                {
+                    Console.WriteLine(" {0}", subscription.EmailAddress);
+                }
             }
             Console.ReadLine();
-            
+
+            var dates = new DateTimeRange(SampleData.Start, SampleData.End);
+            //var results = from date in dates
+            //            join defect in SampleData.AllDefects 
+            //            on date equals defect.Created.Date
+            //            into defectDates
+            //            select new { Date = date, Count = defectDates.Count() };
+
+            var results = dates.GroupJoin(SampleData.AllDefects, 
+                                         date => date, 
+                                         defect => defect.Created.Date, 
+                                         (date, joined) => new { Date = date, Count = joined.Count() });
+            foreach (var grouped in results)
+            {
+                Console.WriteLine("{0:d}: {1}", grouped.Date, grouped.Count);
+            }
+
+            Console.ReadLine();
+
         }       
     }
 }
