8700e5bd50838f18abc2f81301fc5abb22ae1a68
pcastellanos <pcastellanos90@gmail.com>
2016-02-28 13:13:14 -0500

    The compiler translating query expressions into normal C#

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -10,15 +10,52 @@ using System.Xml.Linq;
 
 namespace CSharpTraining
 {
+    static class Extensions
+    {
+        public static Dummy<T> Where<T>(this Dummy<T> dummy,
+                                        Func<T, bool> predicate)
+        {
+            Console.WriteLine("Where called");
+            return dummy;
+        }
+    }
+
+    class Dummy<T>
+    {
+        public Dummy<U> Select<U>(Func<T, U> selector)
+        {
+            Console.WriteLine("Select called");
+            return new Dummy<U>();
+        }
+    }
+
     class Program
     {
         
         public static void Main(string[] args)
         {
-           
+            var query = from user in SampleData.AllUsers
+                        select user;
+            foreach (var user in query)
+            {
+                Console.WriteLine(user);
+            }
             Console.ReadLine();
 
+            //var source = new Dummy<string>();
+
+            //var result = from dummy in source
+            //            where dummy.ToString() == "Ignored"
+            //            select "Anything";
+
+            //Console.ReadLine();
+
         }
+
+
+
+      
+
 
     }
 }
