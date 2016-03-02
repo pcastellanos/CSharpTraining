d3e368dd4059bc5ffb97b6954f1cba06b5da06fa
pcastellanos <pcastellanos90@gmail.com>
2016-03-01 21:39:35 -0500

    Query continuations

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -16,36 +16,41 @@ namespace CSharpTraining
         {
             var query = from defect in SampleData.AllDefects
                         where defect.AssignedTo != null
-                        group defect by defect.AssignedTo;
+                        group defect by defect.AssignedTo into grouped
+                        select new
+                        {
+                            Assignee = grouped.Key,
+                            Count = grouped.Count()
+                        };
 
             foreach (var entry in query)
             {
-                Console.WriteLine(entry.Key.Name);
-                foreach (var defect in entry)
-                {
-                    Console.WriteLine(" ({0}) {1}",
-                    defect.Severity, defect.Summary);
-                }
-                Console.WriteLine();
+                Console.WriteLine("{0}: {1}",
+                entry.Assignee.Name, entry.Count);
             }
             
             Console.ReadLine();
 
-            var defects =  SampleData.AllDefects.Where(defect => defect.AssignedTo != null)
-                                .GroupBy(defect => defect.AssignedTo, defect => defect);
+            //var defects = from grouped in (from defect in SampleData.AllDefects
+            //                               where defect.AssignedTo != null
+            //                               group defect by defect.AssignedTo)
+            //              select new { Assignee = grouped.Key, Count = grouped.Count() };
+
+            var defects = SampleData.AllDefects.Where(defect => defect.AssignedTo != null)
+                                               .GroupBy(defect => defect.AssignedTo).
+                                               Select(grouped => new { Assignee = grouped.Key, Count = grouped.Count() });
 
             foreach (var entry in defects)
             {
-                Console.WriteLine(entry.Key.Name);
-                foreach (var defect in entry)
-                {
-                    Console.WriteLine(" ({0}) {1}",
-                    defect.Severity, defect.Summary);
-                }
-                Console.WriteLine();
+                Console.WriteLine("{0}: {1}",
+                entry.Assignee.Name, entry.Count);
             }
 
             Console.ReadLine();
+
+            
         }
+
+  
     }
 }
