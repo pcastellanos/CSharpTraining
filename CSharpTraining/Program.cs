d93f56c32c3944c9a13765bcc042f5a990e31e9a
pcastellanos <pcastellanos90@gmail.com>
2016-03-13 12:45:57 -0500

    Clean Solution to work on features C# 5

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -1,12 +1,6 @@
-﻿using CSharpTraining.Model;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using System.Runtime.CompilerServices;
 
 namespace CSharpTraining
 {
@@ -14,41 +8,14 @@ namespace CSharpTraining
     {
         public static void Main(string[] args)
         {
-            var query = from defect in SampleData.AllDefects
-                        where defect.AssignedTo != null
-                        group defect by defect.AssignedTo into grouped
-                        select new
-                        {
-                            Assignee = grouped.Key,
-                            Count = grouped.Count()
-                        } into result
-                        orderby result.Count 
-                        select result;
-
-            foreach (var entry in query)
-            {
-                Console.WriteLine("{0}: {1}",
-                entry.Assignee.Name,
-                entry.Count);
-            }
-
-            Console.ReadLine();
-            var defects = SampleData.AllDefects.Where(defect => defect.AssignedTo != null)
-                                    .GroupBy(defect => defect.AssignedTo)
-                                    .Select(grouped => new {Assignee = grouped.Key,Count = grouped.Count()})
-                                    .OrderBy(result => result.Count);
-
-            foreach (var entry in defects)
-            {
-                Console.WriteLine("{0}: {1}",
-                entry.Assignee.Name,
-                entry.Count);
-            }
+         
             Console.ReadLine();
 
             
         }
 
-  
+      
+
+
     }
 }
