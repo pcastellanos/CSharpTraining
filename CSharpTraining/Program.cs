163af23f08ea821aa2bfa91e54e0eba126039e9e
pcastellanos <pcastellanos90@gmail.com>
2016-03-13 12:46:53 -0500

    16.2.1 Basic behavior - Using caller information attributes properly, and abusing them

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -8,14 +8,23 @@ namespace CSharpTraining
     {
         public static void Main(string[] args)
         {
-         
+            ShowInfo();
+            ShowInfo("LiesAndDamnedLies.java", -10);
+            NewCallerMember();
             Console.ReadLine();
 
-            
+
         }
 
-      
+        private static void NewCallerMember()
+        {
+            ShowInfo();
+        }
 
+        static void ShowInfo([CallerFilePath] string file = null, [CallerLineNumber] int line = 0, [CallerMemberName] string member = null)
+        {
+            Console.WriteLine("{0}:{1} - {2}", file, line, member);
+        }
 
     }
 }
