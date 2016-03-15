dc19fab9371251e894d91db2dc956edb479f7a2c
pcastellanos <pcastellanos90@gmail.com>
2016-03-15 16:17:20 -0500

    Exception Filtering

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -11,26 +11,69 @@ namespace CSharpTraining
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            WriteLine("I'm " + person.FullName);
-            WriteLine(person.BuySomething(100));
-            WriteLine(Person.AddNumber(2,3));
+            Before();
+            ReadLine();
+            After();
             ReadLine();
         }
-        
-    }
-    public class Person
-    {
-        public string FirstName { get; } = "Fiyaz";
-        public string LastName { get; } = "Hasan";
 
-        public int Salary { get; private set; } = 3000000;
-        /* Expression bodied function */
-        public int BuySomething(int price) => Salary -= price;
-        public static int AddNumber(int x, int y) => x + y;
-        
-        /* Expression bodied computed property */
-        public string FullName => $"{FirstName} {LastName}";
+        private static void Before()
+        {
+            Random random = new Random();
+            var randomExceptions = random.Next(400, 405);
+            WriteLine("Generated exception: " + randomExceptions);
+            Write("Exception type: ");
+
+            try
+            {
+                throw new Exception(randomExceptions.ToString());
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Equals("400"))
+                    Write("Bad Request");
+                else if (ex.Message.Equals("401"))
+                    Write("Unauthorized");
+                else if (ex.Message.Equals("402"))
+                    Write("Payment Required");
+                else if (ex.Message.Equals("403"))
+                    Write("Forbidden");
+                else if (ex.Message.Equals("404"))
+                    Write("Not Found");
+            }
+        }
+        private static void After()
+        {
+            Random random = new Random();
+            var randomExceptions = random.Next(400, 405);
+            WriteLine("Generated exception: " + randomExceptions);
+            Write("Exception type: ");
+
+            try
+            {
+                throw new Exception(randomExceptions.ToString());
+            }
+            catch (Exception ex) when (ex.Message.Equals("400"))
+            {
+                Write("Bad Request");
+            }
+            catch (Exception ex) when (ex.Message.Equals("401"))
+            {
+                Write("Unauthorized");
+            }
+            catch (Exception ex) when (ex.Message.Equals("402"))
+            {
+                Write("Payment Required");
+            }
+            catch (Exception ex) when (ex.Message.Equals("403"))
+            {
+                Write("Forbidden");
+            }
+            catch (Exception ex) when (ex.Message.Equals("404"))
+            {
+                Write("Not Found");
+            }
+        }
     }
 }
 
