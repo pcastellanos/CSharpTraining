a55238d668b6290537a30141fc82fa799d90c067
pcastellanos <pcastellanos90@gmail.com>
2016-04-10 18:37:37 -0500

    ConcurrentStack and ConcurrentQueue

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -11,21 +11,40 @@ namespace CSharpTraining
     {
         public static void Main()
         {
-            ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            Task.Run(() =>
-            {
-                bag.Add(42);
-                Thread.Sleep(1000);
-                bag.Add(21);
-            });
-
-            Task.Run(() =>
-            {
-                foreach (int i in bag)
-                    Console.WriteLine(i);
-            }).Wait();
-
+            StackMethod();
             Console.ReadLine();
+            QueueMethod();
+            Console.ReadLine();
+
+        }
+
+        private static void StackMethod()
+        {
+            Console.WriteLine("StackMethod");
+            ConcurrentStack <int> stack = new ConcurrentStack<int>();
+            stack.Push(42);
+            int result;
+
+            if (stack.TryPop(out result))
+                Console.WriteLine($"Popped: {result}");
+
+            stack.PushRange(new int[] { 1, 2, 3 });
+            int[] values = new int[2];
+
+            stack.TryPopRange(values);
+
+            foreach (int i in values)
+                Console.WriteLine(i);
+        }
+
+        private static void QueueMethod()
+        {
+            Console.WriteLine("QueueMethod");
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(42);
+            int result;
+            if (queue.TryDequeue(out result))
+                Console.WriteLine($"Dequeued: {result}");
         }
     }
 }
