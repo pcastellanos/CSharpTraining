f6d0f60d2d5033f10d2f097a93e200a3f4d76acc
pcastellanos <pcastellanos90@gmail.com>
2016-04-10 12:04:22 -0500

    listing 1.1: example thread

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -1,12 +1,33 @@
-﻿namespace CSharpTraining
+﻿using System;
+using System.Threading;
+
+namespace CSharpTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(1000);
+            }
+            thread.Join();
+
+            Console.WriteLine("Both thread finished");
+            Console.ReadLine();
+
         }
-
-
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0},", i);
+                Thread.Sleep(300);
+            }
+        }
     }
 }
 
