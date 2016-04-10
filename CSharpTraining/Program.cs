e0604c00396e38b18f782df8f31439df7999f6d6
pcastellanos <pcastellanos90@gmail.com>
2016-04-10 12:16:25 -0500

    listing 1.2 foreground and background threads

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -8,26 +8,19 @@ namespace CSharpTraining
         public static void Main()
         {
             Thread thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.IsBackground = false;
             thread.Start();
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine("Main thread: Do some work.");
-                Thread.Sleep(1000);
-            }
-            thread.Join();
-
-            Console.WriteLine("Both thread finished");
-            Console.ReadLine();
-
         }
+
         public static void ThreadMethod()
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("ThreadProc: {0},", i);
-                Thread.Sleep(300);
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
             }
         }
+
     }
 }
 
