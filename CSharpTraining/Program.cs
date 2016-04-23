c7dab2c8d814260e8cc4d0b33f879fcb478a944f
pcastellanos <pcastellanos90@gmail.com>
2016-04-23 16:08:39 -0500

    Using keyword new in a Method

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -8,14 +8,36 @@ namespace CSharpTraining
         public static void Main()
         {
 
-            MoveableOject obj = new MoveableOject();
-            ((ILeft)obj).Move();
-            ((IRight)obj).Move();
+            Base b = new Base();
+            b.Execute();
+            Base b1 = new Derived();
+            b1.Execute();
             Console.ReadLine();
 
+            Derived b2 = new Derived();
+            b2.Method();
+            Console.ReadLine();
         }
 
     }
+
+    class Base
+    {
+        public void Execute() { Console.WriteLine("Base.Execute"); }
+    }
+    class Derived : Base
+    {
+     
+        public void Method()
+        {
+            this.Execute();
+        }
+        public new void Execute()
+        {
+            Console.WriteLine("Derived.Execute");
+        }
+    }
+
 }
 
 
