c68b90b7aa76cddf065e4e1549c78fcadab3293e
pcastellanos <pcastellanos90@gmail.com>
2016-05-22 16:18:37 -0500

    Web request and response

--- CSharpTraining/Program.cs
+++ CSharpTraining/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace CSharpTraining
@@ -11,52 +12,17 @@ namespace CSharpTraining
     {
         public static void Main()
         {
-            UsingGZipStream();
+            WebRequest request = WebRequest.Create("http://www.microsoft.com");
+            WebResponse response = request.GetResponse();
+            StreamReader responseStream = new StreamReader(response.GetResponseStream());
+            
+            string responseText = responseStream.ReadToEnd();
+            Console.WriteLine(responseText); // Displays the HTML of the website
+            response.Close();
             Console.ReadLine();
-            UsingBufferedStream();
-            Console.ReadLine();
         }
 
-        private static void UsingBufferedStream()
-        {
-            string path  = Path.Combine(Directory.GetCurrentDirectory(), "bufferedStream.txt");
-            using (FileStream fileStream = File.Create(path))
-            {
-                using (BufferedStream bufferedStream = new BufferedStream(fileStream))
-                {
-                    using (StreamWriter streamWriter = new StreamWriter(bufferedStream))
-                    {
-                        streamWriter.WriteLine("A line of text.");
-                    }
-                }
-            }
-            Console.WriteLine("end of buffered example");
-        }
-
-        private static void UsingGZipStream()
-        {
-            string folder = Directory.GetCurrentDirectory();
-            string uncompressedFilePath = Path.Combine(folder, "uncompressed.dat");
-            string compressedFilePath = Path.Combine(folder, "compressed.gz");
-            byte[] dataToCompress = Enumerable.Repeat((byte)'a', 1024 * 1024).ToArray();
-            using (FileStream uncompressedFileStream = File.Create(uncompressedFilePath))
-            {
-                uncompressedFileStream.Write(dataToCompress, 0, dataToCompress.Length);
-            }
-
-            using (FileStream compressedFileStream = File.Create(compressedFilePath))
-            {
-                using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
-                {
-                    compressionStream.Write(dataToCompress, 0, dataToCompress.Length);
-                }
-            }
-
-            FileInfo uncompressedFile = new FileInfo(uncompressedFilePath);
-            FileInfo compressedFile = new FileInfo(compressedFilePath);
-            Console.WriteLine(uncompressedFile.Length); // Displays 1048576
-            Console.WriteLine(compressedFile.Length); // Displays 1052
-        }
+   
     }
 }
 
